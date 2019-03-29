20281ffac80ed424b92243adb9e8606346517b9a
hou <2421771758@qq.com>
2019-03-29 20:00:17 +0800

    1

--- hospitalmanage/src/com/wed/user/UserWed.java
+++ hospitalmanage/src/com/wed/user/UserWed.java
@@ -2,18 +2,16 @@ package com.wed.user;
 
 import java.util.List;
 
-
-import javax.servlet.http.HttpServletRequest;
+import javax.mail.Flags.Flag;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSONArray;
 import com.po.Employee;
-import com.po.Role;
-import com.service.system.RoleService;
 import com.service.system.UserService;
 
 
@@ -22,15 +20,9 @@ import com.service.system.UserService;
 public class UserWed {
 	@Autowired
 	private UserService userService;
-	@Autowired
-	private RoleService roleService;
-	
-	//初始页面
-	@RequestMapping(value="toinitial")
-	public String toinitial(HttpServletRequest request){
-		System.out.println("dddddd");
-		return "/user/initial";
-	}
+
+
+
 	/**
 	 * 验证用户名是否存在
 	 * @param username
@@ -39,6 +31,7 @@ public class UserWed {
 	@ResponseBody
 	@RequestMapping(value="/verify", produces="application/json")
 	public Object verify(String employeecode){
+		System.out.println("ffff");
 		Employee e=new Employee();
 		e.setEmployeecode(employeecode);
 		String valid="true";
@@ -51,52 +44,22 @@ public class UserWed {
 	}
 	
 	/**
-	 * 登录密码
+	 * 登录
 	 * @param bo
 	 * @return
 	 */
 	@ResponseBody
-	@RequestMapping(value="/login", produces="application/json;charset=utf-8")
-	public Object login( Employee bo,HttpServletRequest request){	
-	
+	@RequestMapping(value="/login", produces="application/json")
+	public Object login(Employee bo){	
+		System.out.println("ddd");
 		String msg = "";
 		Employee  em = userService.selectEmployee(bo);
 		if(em != null){
-		 //把权限存到session里面 positionid
-			Role r= roleService.rolePositionid(em.getId());
-			//保存用户的权限
-			request.getSession().setAttribute("role", r);
-			//保持用户的信息
-			request.getSession().setAttribute("employye", em);
-			
 			msg = "1";
 		}else{
 			msg = "密码错误，请重新输入！";
 		}
 		return JSONArray.toJSONString(msg);
-	}
-	//登录跳转的页面
-	@RequestMapping("/toindex")
-	public String tologin(HttpServletRequest request){
-		Object obj=request.getSession().getAttribute("employye");
-		//判断是否登录
-		if(obj != null){
-			System.out.println("ddd");
-			return "/user/index";
-		}
-		//清除session
-		System.out.println("gggg");
-		request.getSession().invalidate();
-		return "/user/login";
-	}
-	
-	
-	//退出
-	@RequestMapping(value="loginout")
-	public String loginout(HttpServletRequest request){
-		//清除session
-		request.getSession().invalidate();
-		return "/user/login";
 	}
 	
 }
